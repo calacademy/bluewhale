73347dcac8ef257d3364d4569b12b8c6d02cf0fc
Greg Rotter <grotter@gmail.com>
2018-07-06 09:27:47 -0700

    revised attract video

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var BLUEWHALE_CONFIG = {
 	isDev: ($.trim(window.location.hash) == '#dev'),
 	idleSeconds: 45,
-	withAttract: false,
+	withAttract: true,
 	legend: {"appetite":{"left":"51px","top":"444px"},"mouth":{"left":"440px","top":"626px"},"blowhole":{"left":"625px","top":"23px"},"heart":{"left":"831px","top":"56px"},"size":{"left":"1175px","top":"58px"},"speed":{"left":"1045px","top":"660px"}},
 	points: {"appetite":{"left":"202px","top":"288px"},"mouth":{"left":"502px","top":"425px"},"blowhole":{"left":"733px","top":"286px"},"heart":{"left":"938px","top":"304px"},"size":{"left":"1285px","top":"191px"},"speed":{"left":"1684px","top":"322px"}}
 };
